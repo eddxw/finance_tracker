ee36bbdc79a19978523048786d5e1211b4bb3190
eddxw <eduardo.jemb@gmail.com>
2019-01-08 17:57:02 -0700

    refactor errors into flash.now messages and rendering partial

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -6,15 +6,19 @@ class StocksController < ApplicationController
       @stock = Stock.new_from_lookup(params[:stock])
       if @stock
         respond_to do |format|
-          format.js {render partial: 'users/result'}
+          format.js { render partial: 'users/result' }
         end
       else
-        flash[:danger] = 'You have entered an incorrect symbol'
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          flash.now[:danger] = 'You have entered an incorrect symbol'
+          format.js { render partial: 'users/result' }
+        end
       end
     else
-      flash[:danger] = 'You have entered an empty search string'
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        flash.now[:danger] = 'You have entered an empty search string'
+        format.js { render partial: 'users/result' }
+      end
     end
   end
 end
