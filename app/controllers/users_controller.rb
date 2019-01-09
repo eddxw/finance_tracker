530616f227e5e7473a4cb63c92ff8a81c7e224d7
eddxw <eduardo.jemb@gmail.com>
2019-01-08 22:41:24 -0700

    add UserStocks destroy acion, + hirb,awesome_print and bullet gems

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@
 
 # UsersController
 class UsersController < ApplicationController
-  def my_portfolio; end
+  def my_portfolio
+    @user = current_user
+    @user_stocks = current_user.stocks
+  end
 end
