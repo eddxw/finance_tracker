1234e3db7a5bd7fca4d23c22d7f12498fe66b854
eddxw <eduardo.jemb@gmail.com>
2019-01-09 23:11:36 -0700

    Add show user, add & remove friendships

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
 
   resources :user_stocks, only: %i[create destroy]
+  resources :users, only: %i[show]
+  resources :friendships
+  post 'add_friend', to: 'users#add_friend'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
